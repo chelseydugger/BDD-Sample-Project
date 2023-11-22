6b6ce7b42775e7963b20841b3391bffdcccb406c
Chelsey Dugger <chelsey.dugger@onbe.com>
2023-11-22 10:06:34 -0600

    Fix Feature File Step Definitions

---
+++ BehavePro_LoginStepDefinitions.cs
@@ -0,0 +1,57 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SampleProject
+{
+    [Binding]
+    public class BehavePro_LoginStepDefinitions
+    {
+        [Given(@"a user has valid credentials")]
+        public void GivenAUserHasValidCredentials()
+        {
+
+        }
+
+        [When(@"the user navigates to the website")]
+        public void WhenTheUserNavigatesToTheWebsite()
+        {
+
+        }
+
+        [When(@"the user enters their credentials")]
+        public void WhenTheUserEntersTheirCredentials()
+        {
+
+        }
+
+        [When(@"the user presses Sign In")]
+        public void WhenTheUserPressesSignIn()
+        {
+
+        }
+
+        [Then(@"the user should be granted access to the site")]
+        public void ThenTheUserShouldBeGrantedAccessToTheSite()
+        {
+
+        }
+
+        [Given(@"a user has invalid credentials")]
+        public void GivenAUserHasInvalidCredentials()
+        {
+
+        }
+
+        [Then(@"the user should not be granted access to the site")]
+        public void ThenTheUserShouldNotBeGrantedAccessToTheSite()
+        {
+
+        }
+
+        [Then(@"the site should display an error message")]
+        public void ThenTheSiteShouldDisplayAnErrorMessage()
+        {
+
+        }
+    }
+}
